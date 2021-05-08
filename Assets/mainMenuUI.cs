b72b4e1981d09e465d2164b60fca3d4b9db9426a
Nicolas Rodrigue <nyx.rodrigue@yahoo.ca>
2021-05-07 23:59:21 -0400

    Logique des boutons du menu principal
    J'ai ajouté du code aux boutons pour qu'ils ouvrent et ferment les différents menus du menu principal

---
+++ Assets/mainMenuUI.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainMenuUI : MonoBehaviour
+{
+	public GameObject menuPrincipal;
+	public GameObject menuOptions;
+	public GameObject menuChoixNiveau;
+	public GameObject menuCommentJouer;
+
+	public GameObject[] listePagesHowToPlay;
+
+	int pageIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+	public void onOptionPressed()
+	{
+		menuPrincipal.SetActive(false);
+		menuOptions.SetActive(true);
+	}
+
+	public void onPlayPressed()
+	{
+		menuPrincipal.SetActive(false);
+		menuChoixNiveau.SetActive(true);
+
+	}
+
+	public void onHowToPlayPressed()
+	{
+
+		menuPrincipal.SetActive(false);
+		menuCommentJouer.SetActive(true);
+		listePagesHowToPlay[0].SetActive(true);
+	}
+
+
+	public void onPagePrecedantePressed()
+	{
+		listePagesHowToPlay[pageIndex].SetActive(false);
+
+		pageIndex -= 1;
+
+		if(pageIndex < 0)
+		{
+			pageIndex = 0;
+		}
+
+		listePagesHowToPlay[pageIndex].SetActive(true);
+	}
+
+	public void onPageSuivantePressed()
+	{
+		listePagesHowToPlay[pageIndex].SetActive(false);
+
+		pageIndex += 1;
+
+		if (pageIndex > listePagesHowToPlay.Length - 1)
+		{
+			pageIndex = listePagesHowToPlay.Length - 1;
+		}
+
+		listePagesHowToPlay[pageIndex].SetActive(true);
+	}
+
+
+	public void onRetourFromCommentJouerPressed()
+	{
+		menuPrincipal.SetActive(true);
+		menuCommentJouer.SetActive(false);
+	}
+
+	public void onRetourFromOptionsPressed()
+	{
+		menuPrincipal.SetActive(true);
+		menuOptions.SetActive(false);
+	}
+
+	public void onRetourFromJouerPressed()
+	{
+		menuPrincipal.SetActive(true);
+		menuChoixNiveau.SetActive(false);
+	}
+}
